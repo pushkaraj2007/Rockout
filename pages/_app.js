db2d586e1f5e56f65c380c228325a4d520f70173
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-03-31 23:21:50 +0530

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import '@/styles/game.css'
+import Header from '../components/header'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
